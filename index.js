0163eddb8e741c9d9c06bc2226784676cc2f5b56
pazj <pazjimenez96@gmail.com>
2022-09-08 09:51:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ socialLinks.forEach((link) => {
 });
 
 // custom cursor
-const cursor = document.querySelector(".cursor");
-window.onmousemove = (e) => {
-  cursor.setAttribute(
-    "style",
-    `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`
-  );
-};
+// const cursor = document.querySelector(".cursor");
+// window.onmousemove = (e) => {
+//   cursor.setAttribute(
+//     "style",
+//     `top: ${e.pageY}px; left: ${e.pageX}px; z-index: 2;`
+//   );
+// };
 
 // navigation
 const tl = gsap.timeline({ paused: true, reversed: true });
